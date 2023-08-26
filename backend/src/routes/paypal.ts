8ac2a207af2ce7ba3b87c07c1fce71bfcbdb4a9f
joabe <joabe9999pereira@gmail.com>
2023-08-26 07:30:16 -0300

    fix- testing railway

--- backend/src/routes/paypal.ts
+++ backend/src/routes/paypal.ts
@@ -7,4 +7,6 @@ router.post('/capture', controller.paypal.captureOrder);
 
 router.post('/create', controller.paypal.createOrder);
 
+router.get("/", (req, res) => {res.status(200).json({mensage: "Hello, im working"})} )
+
 export default router;
