31a89d73fa8d7229bada8385cbeafe886e2038af
joabe <joabe9999pereira@gmail.com>
2023-08-26 05:45:33 -0300

    feat- form fields connected to backend

--- backend/app.ts
+++ backend/app.ts
@@ -1,11 +1,13 @@
 import express, { Express } from 'express';
 import routes from './src/routes';
-import cors from 'cors';
+import cors from "cors"
+
 
 const app: Express = express();
-app.use(cors());
 
 app.use(express.json());
+
+app.use(cors())
 
 app.use("/paypal/order", routes.paypal)
 
